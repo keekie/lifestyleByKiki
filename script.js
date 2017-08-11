9b699e6c6f49a68f93671ef11d76f09ee201238e
keekie <30829439+keekie@users.noreply.github.com>
2017-08-11 23:12:08 +0700

    upd

--- script.js
+++ script.js
@@ -143,3 +143,7 @@ ShopifyBuy.UI.onReady(client).then(function(ui){
 
   })(jQuery);
 });
+
+if (window.width > 479) {
+  $('head').append('<script id="intergram" type="text/javascript" src="https://www.intergram.xyz/js/widget.js"></script>');
+}
